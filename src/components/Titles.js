8e7d50acc2ff0dafa1a8d9ec12bd88a6d11c69f1
Hasal Fernando <witty.leo98@gmail.com>
2019-03-14 23:23:14 +0530

    Titles.js added

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Titles extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Weather Buddy</h1>
+                <p>Find out temperature and humidity</p>
+            </div>
+        );
+    }
+}
