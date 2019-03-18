78b2b5b00f269aad50be9d39a584c9e1cfb3581f
Hasal Fernando <witty.leo98@gmail.com>
2019-03-18 11:19:45 +0530

    States added and the setStates is also done

--- src/App.js
+++ src/App.js
@@ -8,13 +8,31 @@ import './App.css';
 const API_KEY = "430ebea779a86b936979675e08793d51";
 
 class App extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data)
+    console.log(data);
+    
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description
+    })
   }
   
     render() {
